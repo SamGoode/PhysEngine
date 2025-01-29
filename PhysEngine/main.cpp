57dbe367943525f9e0887fdf96ae8e123a3c10e7
SamGoode <samgoode1523@gmail.com>
2025-01-29 16:34:42 +1100

    Shifted main logic into Simulation class

--- PhysEngine/main.cpp
+++ PhysEngine/main.cpp
@@ -40,38 +40,26 @@ public:
     Vector2 prevPos;
     Vector2 pos;
 
-    Vector2 vel;
-    Vector2 acc;
     Vector2 cumulativeImpulse;
-    //Vector2 impulse;
 
     float radius;
     float mass;
 
 public:
+    RigidCircle() {}
+
     RigidCircle(Vector2 _pos, float _radius, float _mass) {
         prevPos = _pos;
         pos = _pos;
-        vel = { 0.f, 0.f };
-        acc = { 0.f, 0.f };
 
-        //cumulativeForce = { 0.f, 0.f };
         cumulativeImpulse = { 0.f, 0.f };
 
         radius = _radius;
         mass = _mass;
     }
 
-    const Vector2& GetPos() {
-        return pos;
-    }
-
     const Vector2& GetPos() const {
         return pos;
-    }
-
-    const Vector2& GetVel() {
-        return vel;
     }
     
     float GetRadius() {
@@ -90,14 +78,6 @@ public:
         return cumulativeImpulse;
     }
 
-    void ApplyAccel(Vector2 accel) {
-        acc += accel;
-    }
-
-    void ClearAcc() {
-        acc = { 0.f, 0.f };
-    }
-
     void ApplyImpulse(Vector2 impulse) {
         cumulativeImpulse += impulse;
     }
@@ -106,22 +86,9 @@ public:
         pos = newPos;
     }
 
-    void SetVel(Vector2 newVel) {
-        vel = newVel;
-    }
-
     void Update(float DeltaTime) {
-        Vector2 offset = cumulativeImpulse * (1/mass) * (DeltaTime * DeltaTime);
-
-        //std::cout << offset.x << ", " << offset.y << std::endl;
-
-        pos += offset;
 
         cumulativeImpulse = { 0.f, 0.f };
-        //vel += acc * DeltaTime;
-        //pos += vel * DeltaTime;
-
-        //acc = { 0.f, 0.f };
     }
 
     void Draw() {
@@ -138,6 +105,8 @@ private:
     float rotation;
 
 public:
+    StaticRect() {}
+
     StaticRect(Vector2 _pos, float _width, float _height, float _rotation) {
         pos = _pos;
         width = _width;
@@ -205,6 +174,94 @@ public:
     }
 };
 
+class Simulation {
+private:
+    Vector2 gravity = { 0.f, 100.f };
+
+    RigidCircle circles[64];
+    StaticRect rects[64];
+
+    int rigidBodyCount;
+    int staticBodyCount;
+
+public:
+    Simulation() {
+        rigidBodyCount = 0;
+        staticBodyCount = 0;
+
+        circles[0] = RigidCircle({ 900.f, 100.f }, 25.f, 10.f);
+        rigidBodyCount++;
+
+        rects[0] = StaticRect({900.f, 600.f}, 400.f, 50.f, -43.f);
+        staticBodyCount++;
+    }
+
+    void Step(float DeltaTime) {
+        for (int i = 0; i < rigidBodyCount; i++) {
+            RigidCircle& circle = circles[i];
+
+            Vector2 circleVel = circle.pos - circle.prevPos; // fix later?
+            circle.prevPos = circle.pos;
+            circle.pos += circleVel;
+
+            circle.pos += gravity * DeltaTime * DeltaTime;
+
+            if (circle.GetPos().y > 900 - circle.GetRadius()) {
+                Vector2 vel = (circle.pos - circle.prevPos) * (1 / DeltaTime);
+
+                circle.pos.y = 900 - circle.radius;
+
+                circle.ApplyImpulse(Vector2{ 0.f, -vel.y } * circle.mass);
+            }
+
+            for (int n = 0; n < staticBodyCount; n++) {
+                Vector2 collisionNormal;
+
+                StaticRect& rect = rects[n];
+
+                bool isColliding = rect.CheckCollision(circle, collisionNormal);
+
+                if (!isColliding) {
+                    continue;
+                }
+
+                Vector2 vel = (circle.pos - circle.prevPos) * (1 / DeltaTime);
+
+                Vector2 toCircle = circle.GetPos() - rect.GetPos();
+                toCircle = Vector2Rotate(toCircle, -rect.GetRotation() * PI / 180);
+
+                if (collisionNormal.x == 0) {
+                    toCircle.y = (circle.GetRadius() + rect.GetHeight() / 2) * collisionNormal.y;
+                }
+                else {
+                    toCircle.x = (circle.GetRadius() + rect.GetWidth() / 2) * collisionNormal.x;
+                }
+
+                toCircle = Vector2Rotate(toCircle, rect.GetRotation() * PI / 180);
+                circle.SetPos(rect.GetPos() + toCircle);
+
+                Vector2 worldNormal = Vector2Rotate(collisionNormal, rect.GetRotation() * PI / 180);
+                Vector2 normalVel = worldNormal * Vector2DotProduct(vel, worldNormal);
+
+                circle.ApplyImpulse(normalVel * circle.mass);
+            }
+
+            circle.pos += circle.cumulativeImpulse * (1 / circle.mass) * DeltaTime;
+            circle.cumulativeImpulse = { 0.f, 0.f };
+        }
+    }
+
+    void Draw() {
+        for (int i = 0; i < rigidBodyCount; i++) {
+            circles[i].Draw();
+        }
+
+        for (int i = 0; i < staticBodyCount; i++) {
+            rects[i].Draw();
+        }
+    }
+};
+
 int main() {
     int screenWidth = 1800;
     int screenHeight = 900;
@@ -213,103 +270,24 @@ int main() {
 
     SetTargetFPS(240);
 
-    Vector2 gravity = { 0, 100.f };
-
-    RigidCircle circle = RigidCircle({ 900.f, 100.f }, 25.f, 10.f);
-
-    circle.vel = { 0.f, 0.f };
-
-    StaticRect rect = StaticRect({900.f, 600.f}, 400.f, 50.f, -43.f);
+    Simulation sim = Simulation();
 
     while (!WindowShouldClose()) {
         // Updates
         float DeltaTime = GetFrameTime();
 
         Vector2 mouse = GetMousePosition();
-        
-        
 
-        Vector2 circleVel = circle.pos - circle.prevPos;
-        circle.prevPos = circle.pos;
-        circle.pos += circleVel;
-
-        circle.pos += gravity * DeltaTime * DeltaTime;
-
-        if (circle.GetPos().y > 900 - circle.GetRadius()) {
-            //circle.SetPos({ circle.GetPos().x, 900 - circle.GetRadius() });
-            Vector2 vel = (circle.pos - circle.prevPos) * (1/DeltaTime);
-
-            circle.pos.y = 900 - circle.radius;
-
-            circle.ApplyImpulse(Vector2{ 0.f, -vel.y } * circle.mass * 1);
-        }
-
-        //circle.SetPos(mouse);
-
-        Vector2 collisionNormal;
-
-        bool isColliding = rect.CheckCollision(circle, collisionNormal);
-
-        if (isColliding) {
-            Vector2 vel = (circle.pos - circle.prevPos) * (1 / DeltaTime);
-
-            //std::cout << vel.x << ", " << vel.y << std::endl;
-            //circle.ClearAcc();
-
-            Vector2 toCircle = circle.GetPos() - rect.GetPos();
-            toCircle = Vector2Rotate(toCircle, -rect.GetRotation() * PI / 180);
-
-
-            if (collisionNormal.x == 0) {
-                toCircle.y = (circle.GetRadius() + rect.GetHeight() / 2) * collisionNormal.y;
-            }
-            else {
-                toCircle.x = (circle.GetRadius() + rect.GetWidth() / 2) * collisionNormal.x;
-            }
-
-            toCircle = Vector2Rotate(toCircle, rect.GetRotation() * PI / 180);
-
-            circle.SetPos(rect.GetPos() + toCircle);
-
-            Vector2 worldNormal = Vector2Rotate(collisionNormal, rect.GetRotation() * PI / 180);
-
-            //std::cout << worldNormal.x << ", " << worldNormal.y << std::endl;
-
-            Vector2 normalVel = worldNormal * Vector2DotProduct(vel, worldNormal);
-
-            std::cout << normalVel.x << ", " << normalVel.y << std::endl;
-
-            circle.ApplyImpulse(normalVel * circle.mass);
-
-            //circle.SetVel(impulse);
-            //circle.ApplyAccel(impulse * (1/DeltaTime));
-        }
-
-        circle.pos += circle.cumulativeImpulse * (1 / circle.mass) * DeltaTime;
-        circle.cumulativeImpulse = { 0.f, 0.f };
-
-        //DrawText((std::to_string(collisionNormal.x) + ", " + std::to_string(collisionNormal.y)).c_str(), 200, 20, 10, RED);
-
-        //circle.Update(DeltaTime);
+        sim.Step(DeltaTime);
         
         // Drawing
         BeginDrawing();
 
         ClearBackground(RAYWHITE);
 
-        circle.Draw();
-
-        if (isColliding) {
-            rect.Draw(RED);
-        }
-        else {
-        }
-           rect.Draw();
-
+        sim.Draw();
 
         DrawFPS(10, 10);
-
-        //std::cout << circle.  << std::endl;
 
         EndDrawing();
     }
